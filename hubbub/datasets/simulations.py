04a37fb2a7be571de13c0f3b8129320bc001d0ee
Hugo Herter <github.dev@hugoh.org>
2014-05-18 00:28:38 +0200

    Minor additions to SIMPLE_LOG

--- hubbub/datasets/simulations.py
+++ hubbub/datasets/simulations.py
@@ -4,4 +4,7 @@ SIMPLE_LOG = [
     {'date':  datetime(2000, 01, 01, 0, 1, 13)},
     {'date':  datetime(2000, 01, 01, 0, 2, 42)},
     {'date':  datetime(2000, 01, 01, 0, 3, 7)},
+    {'date':  datetime(2000, 01, 01, 0, 3, 17)},
+    {'date':  datetime(2000, 01, 01, 0, 3, 27)},
+    {'date':  datetime(2000, 01, 01, 0, 3, 32)},
     ]
