498b8d61dfc2bc1a0ce3b7bf8906a0f7107e3ed3
Hugo Herter <github.dev@hugoh.org>
2014-08-01 01:24:08 +0200

    Added architecture to .deb build script

--- pkg/build.sh
+++ pkg/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+
 sudo rm -r ubuntu/usr
 
 sudo mkdir -p ubuntu/usr/lib/python3/dist-packages/
+sudo mkdir -p ubuntu/usr/lib/pidgin/
 
 sudo cp -r ../hubbub ubuntu/usr/lib/python3/dist-packages/hubbub
+sudo cp ../../Hubbub-Pidgin/pidgin-2.10.9/libpurple/plugins/hubbub-pidgin.so ubuntu/usr/lib/pidgin/hubbub-pidgin.so
 sudo rm -r `find ubuntu/usr/ | grep "__pycache__$"`
 sudo rm -r `find ubuntu/usr/ | grep "\.pyc$"`
 sudo find ubuntu/usr/ -type d -exec chmod 755 {} \;
@@ -11,13 +14,12 @@ sudo find ubuntu/usr/ -type f -exec chmod 644 {} \;
 sudo chown -R root ubuntu/usr
 sudo chgrp -R root ubuntu/usr
 
-dpkg --build ubuntu
+dpkg --build ubuntu hubbub_0.1-1_`uname -m`.deb
 echo "ready to test ?"
 read
-lintian ubuntu.deb
-
-mv ubuntu.deb hubbub_0.1-1_amd64.deb
+lintian hubbub_0.1-1_`uname -m`.deb
 
 echo "moving to VM"
-sudo cp hubbub_0.1-1_amd64.deb /home/okso/.local/share/lxc/hubbub-build2/rootfs/home/ubuntu/hubbub_0.1-1_amd64.deb
+sudo cp hubbub_0.1-1_`uname -m`.deb /home/okso/.local/share/lxc/hubbub-build2/rootfs/home/ubuntu/hubbub_0.1-1_`uname -m`.deb
 
+cp hubbub_0.1-1_`uname -m`.deb $HOME/web/hubbub_`uname -m`.deb
