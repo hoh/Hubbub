b8c6c3f181c143e218d6b471529554d4a1274246
Hugo Herter <github.dev@hugoh.org>
2014-08-14 11:54:44 +0200

    Added 32bit pkg build

--- pkg/build.sh
+++ pkg/build.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
+ARCH=$1
+
 sudo rm -r ubuntu/usr
 
 sudo mkdir -p ubuntu/usr/lib/python3/dist-packages/
 sudo mkdir -p ubuntu/usr/lib/pidgin/
 
+sudo rm ubuntu/DEBIAN/control
+if [ "$ARCH" == "i386" ]
+then
+    cat control | sed s/amd64/$ARCH/ > ubuntu/DEBIAN/control
+else
+    cp control ubuntu/DEBIAN/control
+fi
+sudo chown root ubuntu/DEBIAN/control
+sudo chgrp root ubuntu/DEBIAN/control
+
 sudo cp -r ../hubbub ubuntu/usr/lib/python3/dist-packages/hubbub
-sudo cp ../../Hubbub-Pidgin/pidgin-2.10.9/libpurple/plugins/hubbub-pidgin.so ubuntu/usr/lib/pidgin/hubbub-pidgin.so
+#sudo cp ../../Hubbub-Pidgin/pidgin-2.10.9/libpurple/plugins/hubbub-pidgin.so ubuntu/usr/lib/pidgin/hubbub-pidgin.so
+sudo cp hubbub-pidgin_$ARCH.so ubuntu/usr/lib/pidgin/hubbub-pidgin.so
+
 sudo rm -r `find ubuntu/usr/ | grep "__pycache__$"`
 sudo rm -r `find ubuntu/usr/ | grep "\.pyc$"`
 sudo find ubuntu/usr/ -type d -exec chmod 755 {} \;
@@ -14,12 +28,12 @@ sudo find ubuntu/usr/ -type f -exec chmod 644 {} \;
 sudo chown -R root ubuntu/usr
 sudo chgrp -R root ubuntu/usr
 
-dpkg --build ubuntu hubbub_0.1-1_`uname -m`.deb
+dpkg --build ubuntu hubbub_0.1-1_$ARCH.deb
 echo "ready to test ?"
 read
-lintian hubbub_0.1-1_`uname -m`.deb
+lintian hubbub_0.1-1_$ARCH.deb
 
 echo "moving to VM"
-sudo cp hubbub_0.1-1_`uname -m`.deb /home/okso/.local/share/lxc/hubbub-build2/rootfs/home/ubuntu/hubbub_0.1-1_`uname -m`.deb
+sudo cp hubbub_0.1-1_$ARCH.deb /home/okso/.local/share/lxc/hubbub-build2/rootfs/home/ubuntu/hubbub_0.1-1_$ARCH.deb
 
-cp hubbub_0.1-1_`uname -m`.deb $HOME/web/hubbub_`uname -m`.deb
+cp hubbub_0.1-1_$ARCH.deb $HOME/web/hubbub_$ARCH.deb
